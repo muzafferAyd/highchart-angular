9eb82ed4c6458467a570374cd72bd678ba0e2223
muzaffer <aydogdum38@gmail.com>
2021-10-30 22:54:18 +0300

    removed dots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,16 @@ export class AppComponent {
   Highcharts: typeof Highcharts = Highcharts; // required
   chartConstructor = 'chart'; // optional string, defaults to 'chart'
   chartOptions: Highcharts.Options = {
+    plotOptions: {
+      series: {
+        marker: {
+          enabled: false
+        }
+      }
+    },
     series: [{
-      data: [1, 2, 3],
-      type: 'line'
+      data: [500, 700, 555, 444, 777, 877, 944, 567, 666, 789, 456, 654],
+      type: 'spline'
     }]
   }; // required
  // chartCallback: Highcharts.ChartCallbackFunction = function(chart) {} // optional function, defaults to null
